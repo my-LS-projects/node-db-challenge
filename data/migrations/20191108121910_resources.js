6943ee42b1be1e59050d5bb32cc062880e55775f
frescocodes <hueydakotah@gmail.com>
2019-11-08 12:49:29 -0500

    migrations fixes

--- data/migrations/20191108121910_resources.js
+++ data/migrations/20191108121910_resources.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('resources', tbl => {
       tbl.increments();
       tbl.string('resource_name', 255).notNullable();
-      tbl.string('description', 255);
+      tbl.string('resource_description', 255);
   })
 };
 
