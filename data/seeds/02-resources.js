59fe0e7406c9f21fd06ddbfa42662979e39d2358
frescocodes <hueydakotah@gmail.com>
2019-11-08 13:55:15 -0500

    MVP: router, model, seeds done

--- data/seeds/02-resources.js
+++ data/seeds/02-resources.js
@@ -1,6 +1,6 @@
 
 exports.seed = function(knex) {
-  return knex('table_name').insert([
+  return knex('resources').insert([
     {resource_name: 'booth', resource_description: 'recording booth'},
     {resource_name: 'keyboard', resource_description: 'mechanical'},
     {resource_name: 'headphones', resource_description: 'sennheiser hd598'},
